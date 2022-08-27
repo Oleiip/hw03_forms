bef1202aebeb98ea4044444b9efcae9633e52fb8
Олейник Павел <oleiip@yandex.com>
2022-08-27 13:41:55 +0300

    unused import delete

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,4 +1,3 @@
-from tokenize import group
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
