cf50c2fbdd0e21c6176548121f4fcd98bb2e83f5
Олейник Павел <oleiip@yandex.com>
2022-08-27 13:18:14 +0300

    3 final edit

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -10,8 +10,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -59,14 +57,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
